ed27f31f02a47928072285da2f528cd3e7b92b26
abedassi2001 <abedassi134@gmail.com>
2025-09-01 06:55:31 +0300

    added usersutility file

---
+++ backend/utils/usersutility.js
@@ -0,0 +1,4 @@
+/*in this file we will write methods that will help us with inserting the user information
+we need to make  function called encrypt that will encrypt the user info , nad a function called
+decrypt that will decrypt the info (password). and other helper functions */
+
