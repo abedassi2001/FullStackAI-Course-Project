41642ddd27c798abec53a1e74ddd68b3f3e3d4ce
abedassi2001 <abedassi134@gmail.com>
2025-09-01 19:51:50 +0300

    added files for the querey back end part

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const usersRouter = require("./routers/usersRouter");
+const queryRouter = require("./routers/queryRouter");   
 
 const app = express();
 app.use(express.json());
 
 // Users routes
 app.use("/users", usersRouter);
-console.log("hi");
+app.use("/queries", queryRouter);
 module.exports = app;
