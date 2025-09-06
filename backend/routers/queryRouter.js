32d58a67cdf48de23f01e1184ef6a5bc2b7ffa88
abedassi2001 <abedassi134@gmail.com>
2025-09-06 17:32:34 +0300

    the backend of query is good now , we need the ai

--- backend/routers/queryRouter.js
+++ backend/routers/queryRouter.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const queryController = require("../controllers/queryController");
+const { upload } = require("../middlewares/queryMiddleware"); // import upload
 
-router.post("/", queryController.create);
+router.post("/", upload.single("dbfile"), queryController.create);
 router.get("/", queryController.getAll);
 
 module.exports = router;
